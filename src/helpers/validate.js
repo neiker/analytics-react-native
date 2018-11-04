fb3b99162e7a660209638bff5e9eced094ab9419
Javier Alvarez <neiker@gmail.com>
2018-11-04 12:41:47 -0300

    Swallow exceptions on production (#167)

--- src/helpers/validate.js
+++ src/helpers/validate.js
@@ -48,26 +48,95 @@ const rules = [
   },
 ];
 
+const asserts = {
+  identify: (message) => {
+    assert(
+      message.anonymousId || message.userId,
+      'You must pass either an `anonymousId` or a `userId`.',
+    );
+  },
+  group: (message) => {
+    assert(
+      message.anonymousId || message.userId,
+      'You must pass either an `anonymousId` or a `userId`.',
+    );
+    assert(
+      message.groupId,
+      'You must pass a `groupId`.',
+    );
+  },
+  track: (message) => {
+    assert(
+      message.anonymousId || message.userId,
+      'You must pass either an `anonymousId` or a `userId`.',
+    );
+    assert(
+      message.event,
+      'You must pass an `event`.',
+    );
+  },
+  page: (message) => {
+    assert(
+      message.anonymousId || message.userId,
+      'You must pass either an `anonymousId` or a `userId`.',
+    );
+  },
+  screen: (message) => {
+    assert(
+      message.anonymousId || message.userId,
+      'You must pass either an `anonymousId` or a `userId`.',
+    );
+  },
+  alias: (message) => {
+    assert(
+      message.userId,
+      'You must pass a `userId`.',
+    );
+    assert(
+      message.previousId,
+      'You must pass a `previousId`.',
+    );
+  },
+};
+
 /**
  * Validate an options `obj`.
  *
  * @param {Object} obj
  */
 
-export default function validate(obj) {
+function validate(message, type) {
   assert(
-    typeOf(obj) === 'object',
+    typeOf(message) === 'object',
     'You must pass a message object.',
   );
 
   rules.forEach((rule) => {
-    if (obj[rule.name]) {
+    if (message[rule.name]) {
       const types = [].concat(rule.types);
 
       assert(
-        types.some(type => typeOf(obj[rule.name]) === type),
+        types.some(t => typeOf(message[rule.name]) === t),
         `"${rule.name}" must be ${types.join(' or ')}.`,
       );
     }
   });
+
+  if (asserts[type]) {
+    asserts[type](message);
+  } else {
+    throw new TypeError('Invalid event type');
+  }
 }
+
+export default (message, type) => {
+  try {
+    validate(message, type);
+  } catch (e) {
+    if (process.env.NODE_ENV === 'production') {
+      return;
+    }
+
+    throw e;
+  }
+};
